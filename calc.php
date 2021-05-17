f0f738d685d66d8f7648b02419ac7b317fce1093
‘“1thegame” <iurii.matkivskyi@gmail.com>
2021-05-17 20:39:23 +0300

    calc

--- calc.php
+++ calc.php
@@ -1,14 +1,10 @@
 <?php
 
-$number1 = fwrite(STDOUT, "Enter first number: ");
-$number1 = fgets(STDIN);
+$number1 = readline("enter a number 1 \n :");
 if (is_numeric($number1)) {
-    $operand = fwrite(STDOUT, "Enter 1 operand: +,-,/,* ");
-    $operand = fgets(STDIN);
+    $operand = readline("enter a operand \n :");
     if ($operand == '+' || $operand == '-' || $operand == '/' || $operand == '*') {
-        echo $operand . "\n";
-        $number2 = fwrite(STDOUT, "Enter second number: ");
-        $number2 = fgets(STDIN);
+        $number2 = readline("second number \n :");
         switch ($operand) {
             case '-':
                 $result = $number1 - $number2;
@@ -30,11 +26,12 @@ if (is_numeric($number1)) {
             default:
                 echo "something wrong \n";
         }
+
     } else {
         echo "Do it again noob!and Only choose one of this operators : +,-,/,* \n";
     }
 }else {
-    echo "Only numbers!!you need to start from begiingn \n";
+    echo "Only numbers!!you need to start from begining \n";
 
 }
 
